91bc7e461d48245631d6fe3349aa8a6a6a42cdf1
Karol-Bobryk <karolekb2002@gmail.com>
2024-07-26 01:18:03 +0200

    added hexdump

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +9,7 @@
 
 int main() {
 
-  FILE *image = fopen("./resources/img1.jpeg", "r");
+  FILE *image = fopen("./resources/img1.jpeg", "rb");
 
   if (image == NULL) {
 
@@ -21,26 +22,15 @@ int main() {
   fseek(image, 0, SEEK_SET);
 
   u_int8_t buffer[fileLength];
-  size_t bytesRead = fread(buffer, 8, fileLength / 8, image);
+  size_t bytesRead = fread(buffer, 8, fileLength, image);
+  u_int8_t additionalBytes = 0;
 
-  for (int i = 0; i < fileLength / 8; i++) {
-
+  for (int i = 0; i < fileLength; i++) {
     if (i != 0 && i % 12 == 0) {
       printf("\n");
     }
     printf("%02X, ", buffer[i]);
   }
-
-  size_t bytesLost = fileLength - bytesRead * 8;
-  printf("%02X, ", buffer[bytesRead - 1]);
-  // TODO: Fix this shit tommorow
-  //     /\  /|
-  //    / \_/ |
-  //   | *  * /
-  //   \__^__/
-  //
-  printf("\n");
-  printf("bytes lost: %zu\n", bytesLost);
 
   fclose(image);
 
